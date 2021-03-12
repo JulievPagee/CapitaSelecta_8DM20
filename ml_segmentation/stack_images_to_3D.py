d16180d2c48594d1b6176a8ad67a6930698f6e66
anouk610 <54893704+anouk610@users.noreply.github.com>
2021-03-12 16:43:24 +0100

    code voor 2D naar 3D

--- ml_segmentation/stack_images_to_3D.py
+++ ml_segmentation/stack_images_to_3D.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 12/03/2021
+
+@author: 20165272
+"""
+import cv2
+import numpy as np
+import SimpleITK as sitk
+from PIL import Image
+import pylab as plt
+from glob import glob
+import argparse
+import os
+import progressbar
+import pickle as pkl
+from numpy.lib import stride_tricks
+from skimage import feature
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import time
+import mahotas as mt
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+import scipy.ndimage as ndimage
+import pandas as pd
+
 #option 1
 #ref: https://stackoverflow.com/questions/31573872/how-to-read-multiple-images-and-create-a-3d-matrix-with-them
 #arrays = []
@@ -23,20 +50,30 @@
 
 #doel (333,271,86) array maken.
 #option 2 lijkt me het handiste
-#data_list = [ 'insert datanames here']
-#save_path
+def slice_to_3D(data_list, save_path):
 
-def 2D_to_3D(data_list, save_path)
-#data_list = [ 'insert datanames here']
-#save_path meegeven
     final_image = np.array([])
+    final_image = np.zeros((333,271,86))
     firstIteration = True
     for item in data_list:
+        img = data_path+ patient_path + item + '.png'
+        img = sitk.ReadImage(img)
+        img = sitk.GetArrayFromImage(img)
         if firstIteration:
-            final_image = item
+            final_image = img
             firstIteration=False
         else:
-            final_image = np.concatenate(([item , final_image ]), axis=2)
+            final_image = np.concatenate(([img, final_image ]), axis=2)
+            #error: axis 2 is out of bounds for array of dimension 2
+            # wanneer ik axis = 1 gebruik komen ze naast elkaar te staan.
 
     final_image= Image.fromarray(final_image)
-    final_image.save(save_path)
+    final_image.show()
+
+data_path = 'C:/Users/20165272/Documents/8DM20 Capita Selecta/Project/ml_segmentation_old/ClassData/Labelled/Slices/'
+patient_path = 'img_p107_slice_'
+data_list = [ '1', '2', '3']
+save_path= 'C:/Users/20165272/Documents/8DM20 Capita Selecta/Project/ml_segmentation_old'
+save_name= 'final_image'
+
+slice_to_3D(data_list,save_path)
